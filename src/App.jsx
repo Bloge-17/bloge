6cc83558dceb8d0a6d87afcc3d7da04c531f0454
GKKoltunov <gkkoltunov@gmail.com>
2024-02-12 16:45:23 +0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.scss";
 import { Header } from "./layout/Header/Header";
+import { Card } from "./layout/components/Card/Card";
+
+const data = {
+  theme: "INTERNET",
+  date: "June 28, 2021",
+  heading: "How to design a product that can grow itself 10x in year",
+  text: "Auctor Porta. Augue vitae diam mauris faucibus blandit elit per, feugiat leo dui orci. Etiam vestibulum. Nostra netus per conubia dolor.",
+  author: {
+    name: "Jenny Wilson",
+    profession: "Product Designer",
+    ava: "./src/images/author.webp",
+  },
+  image: "./src/images/card-post.webp",
+};
+
+
 
 function App() {
 
   return <>
-  <Header/>
+    <Header />
+    <Card data={data } />
   </>;
 }
 
