1292325969397bd707b18010fbcf1195c11edb15
GKKoltunov <gkkoltunov@gmail.com>
2024-02-12 18:54:36 +0300

    cards position

--- src/layout/components/Card/Card.jsx
+++ src/layout/components/Card/Card.jsx
@@ -2,21 +2,27 @@ import "./Card.scss";
 
 import React from "react";
 
-export const Card = ({ data }) => {
+export const Card = ({ image, theme, date, heading, text, author }) => {
+
+
+
+
+  
   return (
+   
     <article className="post">
-      <img className="post__img" src={data.image} alt="pic" />
+      <img className="post__img" src={image} alt="pic" />
       <div className="post__content">
-        <p className="post__theme">{data.theme}</p>
+        <p className="post__theme">{theme}</p>
         <span className="point"></span>
-        <p className="post__date">{data.date}</p>
-        <h3 className="post__heading">{data.heading}</h3>
-        <p className="post__text">{data.text}</p>
+        <p className="post__date">{date}</p>
+        <h3 className="post__heading">{heading}</h3>
+        <p className="post__text">{text}</p>
         <div className="post__author">
-          <img className="post__ava" src={data.author.ava} alt="ava" />
+          <img className="post__ava" src={author.ava} alt="ava" />
           <div>
-            <p className="post__avtor"> {data.author.name}</p>
-            <p className="post__profession">{data.author.profession}</p>
+            <p className="post__avtor"> {author.name}</p>
+            <p className="post__profession">{author.profession}</p>
           </div>
         </div>
       </div>
