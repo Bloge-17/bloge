342d6b7ee46842db166b609447a61d0ab3dd778d
GKKoltunov <gkkoltunov@gmail.com>
2024-02-13 14:38:33 +0300

    context , last date post

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import "./Main.scss";
+import { CardContext } from "../../providers/context/CardContext";
+
+export const Main = () => {
+  const { cards } = useContext(CardContext);
+  return <main>{cards}</main>;
+};
---
+++ src/providers/context/CardProvider.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { CardContext } from "./CardContext";
+import { useState, useEffect } from "react";
+import { Card } from "../../components/Card/Card";
+
+export const CardProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function renderCards() {
+      try {
+        const response = await fetch("http://localhost:3000/cards");
+
+        if (response.ok) {
+          const arr = await response.json();
+          setData(arr);
+        } else {
+          console.log("error");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    renderCards();
+  }, []);
+
+  
+
+  let lastDate = data.map((el) => +(new Date((el.date))))
+ function getMaxOfArray() {
+   return Math.max.apply(null, lastDate);
+ }
+
+  let lastPost = getMaxOfArray() // находим дату последнего поста
+
+
+  let cards = data.map((el) => <Card {...el} lastPost={lastPost} key={el.id} />); // мапаем карточки
+
+
+  return (
+    <CardContext.Provider value={{ cards }}>{children}</CardContext.Provider>
+  );
+};
