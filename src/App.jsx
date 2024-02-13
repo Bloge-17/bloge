133038ddc148b5ea6b8297996798d54d49133bb9
Usmon <noname4v7@gmail.com>
2024-02-13 13:16:17 +0300

    bez  izmineniy

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,34 @@ import { Header } from "./layout/Header/Header";
 import { Card } from "./layout/components/Card/Card";
 import { Main } from "./layout/components/Main/Main";
 
-
-
-
 function App() {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    async function renderCards() {
+      try {
+        const response = await fetch("http://localhost:3000/cards");
 
-const [data, setData] = useState([]);
-
-  
-  
-useEffect(() => {
-  async function renderCards() {
-    try {
-      const response = await fetch("http://localhost:3000/cards");
-
-      if(response.ok){
-      const arr = await response.json();
-      setData(arr);
-      } else {
-        console.log("error")
-     }
-    } catch (e) {
-      console.log(e);
+        if (response.ok) {
+          const arr = await response.json();
+          setData(arr);
+        } else {
+          console.log("error");
+        }
+      } catch (e) {
+        console.log(e);
+      }
     }
-  }
-  renderCards();
-}, []);
-  
-  
+    renderCards();
+  }, []);
 
-let cards = data.map((el) => <Card {...el} key={el.id} />);
+  let cards = data.map((el) => <Card {...el} key={el.id} />);
 
   return (
     <>
       <Header />
       <Main cards={cards} />
     </>
-  )
+  );
 }
 export default App;
