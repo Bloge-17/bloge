b006eb470afc91a82b911e92ddaca9320949a7d0
Usmon <noname4v7@gmail.com>
2024-02-14 12:53:23 +0300

    a

--- src/providers/router/routes.jsx
+++ src/providers/router/routes.jsx
@@ -3,7 +3,6 @@ import { SearchPage } from "../../Pages/SearchPage/SearchPage";
 
 import { HomePage } from "../../Pages/HomePage/HomePage";
 
-
 export const router = createBrowserRouter([
   {
     path: "/",
