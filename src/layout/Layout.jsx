ba8968135d91cf9b9d54065267a3bd23821f1b10
GKKoltunov <gkkoltunov@gmail.com>
2024-02-19 19:14:45 +0300

    modal, add post Co-authored-by: Danil <code0verdose@users.noreply.github.com>

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,14 +3,28 @@ import { Header } from "./Header/Header";
 import { Outlet } from "react-router-dom";
 import { SearchProvider } from "../providers/context/SearchProvider";
 import { CardProvider } from "../providers/context/CardProvider";
+import { Modal } from "../components/Modal/Modal";
+import { useState } from "react";
+
 
 
 export const Layout = ({ children }) => {
+
+ const [isOpen, setIsOpen] = useState(false);
+
+ function CreatePortal() {
+   const portal = document.createElement("div");
+   portal.id = "portal";
+   document.body.append(portal);
+ }
+
+
   return (
     <>
       <SearchProvider>
         <CardProvider>
-          <Header />
+          <Header CreatePortal={CreatePortal} setIsOpen={setIsOpen}/>
+          {isOpen && <Modal setIsOpen={setIsOpen}  />}
           <Outlet />
         </CardProvider>
       </SearchProvider>
